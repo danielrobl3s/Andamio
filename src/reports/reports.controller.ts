8fb36be7a9ba6bc4d5d6c5038a33cdcb91fed737
danielrobl3s <danielrobles200217@gmail.com>
2026-05-29 14:15:27 -0600

    add: reports module CRUD operations

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ModelName } from './interfaces/filters.types';
+import { UpdateReportDto } from './dto/update-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -14,5 +15,32 @@ export class ReportsController {
         @Body() body: CreateReportDto<ModelName>
     ){
         return this.reportsService.create(body);
+    }
+
+    @Get('')
+    async getAllReports(){
+        return this.reportsService.getAll();
+    }
+
+    @Get('/:id')
+    async getReportById(
+        @Param('id') id: string
+    ){
+        return this.reportsService.getOne(id);
+    }
+
+    @Patch('/update/:id')
+    async updateReport(
+        @Param('id') id: string,
+        @Body() body: UpdateReportDto
+    ){
+        return this.reportsService.update(id, body);
+    }
+
+    @Delete('/delete/:id')
+    async deleteReport(
+        @Param('id') id: string
+    ){
+        return this.reportsService.delete(id);
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,9 @@ import { CreateReportDto } from './dto/create-report.dto';
 import { ModelName } from './interfaces/filters.types';
 import { InputJsonValue } from '@prisma/client/runtime/binary';
 import { QueryDto } from './dto/query.dto';
+import { Query } from './interfaces/query.interface';
+import { timingSafeEqual } from 'crypto';
+import { UpdateReportDto } from './dto/update-report.dto';
 
 @Injectable()
 export class ReportsService {
@@ -20,10 +23,42 @@ export class ReportsService {
                 query: createReportDto.query as unknown as InputJsonValue
             }
         });
+        
 
         return {
             ...createdReport,
-            query: createdReport.query as unknown as QueryDto<ModelName>
+            query: createdReport.query as unknown as Query
         }
+    }
+
+    async getAll(){
+        return this.prismaService.report.findMany();
+    }
+
+    async getOne(id: string){
+        return this.prismaService.report.findUnique({
+            where: {id: id}
+        });
+    }
+
+    async update(id: string, updateReportDto: UpdateReportDto){
+        const updatedReport = await this.prismaService.report.update({
+            where: {id: id},
+            data: {
+                ...updateReportDto,
+                query: updateReportDto.query as unknown as InputJsonValue
+            }
+        });
+
+        return {
+            ...updatedReport,
+            query: updatedReport.query as unknown as Query
+        }
+    }
+
+    async delete(id: string){
+        return this.prismaService.report.delete({
+            where: {id: id}
+        })
     }
 }
