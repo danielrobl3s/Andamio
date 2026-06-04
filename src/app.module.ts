dfa119fd0d2f843e5dc71ecdcf1435269175f2d7
danielrobl3s <danielrobles200217@gmail.com>
2026-06-04 14:21:40 -0600

    add: better-auth, token-based authorization and google sign-in

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,21 @@ import { PrismaService } from './prisma/prisma.service';
 import { HhrrWorkersModule } from './hhrr_workers/hhrr_workers.module';
 import { HhrrModule } from './hhrr/hhrr.module';
 import { ReportsModule } from './reports/reports.module';
+import { auth } from './lib/auth';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
 
 @Module({
-  imports: [ProjectsModule, PrismaModule, ProjectsPropertiesModule, ClientsModule, SalesModule, HhrrWorkersModule, HhrrModule, ReportsModule],
+  imports: [
+    ProjectsModule, 
+    PrismaModule, 
+    ProjectsPropertiesModule, 
+    ClientsModule, 
+    SalesModule, 
+    HhrrWorkersModule, 
+    HhrrModule, 
+    ReportsModule,
+    AuthModule.forRoot({auth})
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
