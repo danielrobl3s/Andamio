fe11b6627d0650bdd0295e30ab0aac31b3957357
danielrobl3s <danielrobles200217@gmail.com>
2026-06-22 13:52:54 -0600

    add: CORS frontend url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,12 @@ const prisma = new PrismaClient();
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL!,
-    trustedOrigins: [process.env.BETTER_AUTH_URL!, "http://localhost:3001", "http://localhost:3000"],
+    trustedOrigins: [
+        process.env.BETTER_AUTH_URL!, 
+        "http://localhost:3001", 
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
     plugins: [
         bearer()
     ],
