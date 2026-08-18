a1c5be214571d3fbb75a226f1ed8a720bfa1f023
danielrobl3s <danielrobles200217@gmail.com>
2026-08-18 12:23:14 -0600

    add: missing prismaService imports in contracts and clienLogs modules

--- src/client_logs/client_logs.module.ts
+++ src/client_logs/client_logs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientLogsController } from './client_logs.controller';
 import { ClientLogsService } from './client_logs.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ClientLogsController],
-  providers: [ClientLogsService]
+  providers: [ClientLogsService, PrismaService]
 })
 export class ClientLogsModule {}
